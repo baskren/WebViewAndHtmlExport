74556d8033c461ab9710e9d1d16da6c5e4a2630a
Ben Askren <ben@buildcalc.com>
2019-12-30 17:04:04 -0500

    InitialCommit

---
+++ WebViewAndHtmlExport/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Forms9Patch;
+
+namespace WebViewAndHtmlExport
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            webView.Source = new HtmlWebViewSource
+            {
+                Html = @"
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Convert to PNG</h1>
+
+<p>This html will be converted to a PNG, PDF, or print.</p>
+
+</body>
+</html>
+"
+            };
+
+            shareButton.Clicked += ShareButton_Clicked;
+        }
+
+        async void ShareButton_Clicked(object sender, EventArgs e)
+        {
+            using (var indicator = ActivityIndicatorPopup.Create())
+            {
+                if (await webView.ToPdfAsync("output.pdf") is ToFileResult pdfResult)
+                {
+                    await indicator.PopAsync();
+
+                    if (pdfResult.IsError)
+                        using (Toast.Create("PDF Failure", pdfResult.Result)) { }
+                    else
+                    {
+                        var collection = new Forms9Patch.MimeItemCollection();
+                        collection.AddBytesFromFile("application/pdf", pdfResult.Result);
+                        Forms9Patch.Sharing.Share(collection, shareButton);
+                    }
+                }
+                else
+                    await indicator.PopAsync();
+
+            }
+        }
+    }
+}
